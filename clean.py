5229f6835e0b324ad89bc90857c9f44a7c411a75
helmholtz <arjun.mendiratta@gmail.com>
2016-01-13 16:38:37 -0800

    Added clean script

---
+++ clean.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = pd.read_csv('data.csv', dtype={'zipcode': str})
+
+data=data.drop(range(0,4))  #drop first four rows
+
+#Strip $ and , from amounts and convert to float
+data['amount'] = data['amount'].str[1:].str.replace(",","").astype('float')
+
+data.to_csv('cleaned_data.csv')
+
+
+
+
